ac89d7165b25241fcf9f606c3edb58740e78bf42
SCMNSH Admin <admin@scmnsh.com>
2026-07-31 16:31:02 +0800

    完善后台管理系统：新增领导班子、会长寄语、荣誉历程、大事记、专委会、党委党建、战略伙伴、加入我们等模块

--- data.js
+++ data.js
@@ -1,14 +1,24 @@
-// 四川省闽南商会 - 网站数据文件
+// 四川省闽南商会 - 网站数据文件（由管理后台自动生成）
+// 上传此文件到服务器即可更新网站内容
 window.__CMS_DATA__ = {
   "banner": [],
   "about": { "en": "ABOUT US", "cn": "了解商会", "img": "", "p1": "", "p2": "", "link": "#", "stats": [] },
-  "services": { "desc": "", "items": [] },
+  "leadership": [],
+  "president": { "name": "", "title": "", "img": "", "content": "" },
+  "honors": [],
+  "milestones": [],
+  "committees": [],
+  "party": { "name": "党委", "intro": "", "structImg": "", "structDesc": "", "news": [] },
   "news": [],
   "biznews": [],
+  "services": { "desc": "", "items": [] },
   "partners": [],
+  "strategy": [],
+  "joinus": { "title": "加入我们", "req": "", "process": "", "contact": "" },
   "footer": {
     "title": "四川省闽南商会", "keywords": "", "desc": "",
     "logo": "", "slogan": "如果您有需求，请联系我们；我们将竭诚为您服务！",
-    "email": "", "addr": "", "phone": "", "icp": "", "copyright": "Copyright © 四川省闽南商会 All Rights Reserved"
+    "email": "", "addr": "", "phone": "", "icp": "",
+    "copyright": "Copyright © 四川省闽南商会 All Rights Reserved"
   }
 };
