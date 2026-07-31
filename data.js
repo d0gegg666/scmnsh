9dc5d33ff91372d31305834cc3317399c088d1ae
SCMNSH Admin <admin@scmnsh.com>
2026-07-31 16:15:34 +0800

    初始化四川省闽南商会网站

---
+++ data.js
@@ -0,0 +1,14 @@
+// 四川省闽南商会 - 网站数据文件
+window.__CMS_DATA__ = {
+  "banner": [],
+  "about": { "en": "ABOUT US", "cn": "了解商会", "img": "", "p1": "", "p2": "", "link": "#", "stats": [] },
+  "services": { "desc": "", "items": [] },
+  "news": [],
+  "biznews": [],
+  "partners": [],
+  "footer": {
+    "title": "四川省闽南商会", "keywords": "", "desc": "",
+    "logo": "", "slogan": "如果您有需求，请联系我们；我们将竭诚为您服务！",
+    "email": "", "addr": "", "phone": "", "icp": "", "copyright": "Copyright © 四川省闽南商会 All Rights Reserved"
+  }
+};
